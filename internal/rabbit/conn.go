ad90be3ebe4d69151a983b23c574c70938829618
Manikanta <pise.manikanta@gmail.com>
2020-07-30 12:27:14 +0530

    Add files via upload

---
+++ internal/rabbit/conn.go
@@ -0,0 +1,23 @@
+// Conn -
+package rabbit
+
+import (
+	"github.com/streadway/amqp"
+)
+
+type Conn struct {
+	Channel *amqp.Channel
+}
+
+// GetConn -
+func GetConn(rabbitURL string) (Conn, error) {
+	conn, err := amqp.Dial(rabbitURL)
+	if err != nil {
+		return Conn{}, err
+	}
+
+	ch, err := conn.Channel()
+	return Conn{
+		Channel: ch,
+	}, err
+}
